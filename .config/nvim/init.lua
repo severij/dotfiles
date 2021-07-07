b5b6c1fb544aea6456e88a3fb631dc7c0205378e
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2021-07-07 14:59:59 +0300

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,23 +4,6 @@ require('plugins')
 vim.cmd('filetype plugin indent on')
 vim.cmd('syntax enable')
 
-
-local buf_wipe_interval = 180000 -- in milliseconds
-local time_since_modified_threshold = 1800 -- in seconds
-
-local timer = vim.loop.new_timer()
-timer:start(buf_wipe_interval, buf_wipe_interval, vim.schedule_wrap(function()
-  for _,buf in ipairs(vim.fn['getbufinfo']()) do
-    local time_since_modified =
-        vim.fn['localtime']() - vim.fn['getftime'](buf.name)
-    if buf.hidden == 1 and buf.changed == 0 and 
-        not buf.variables.term_title and not vim.b.keep_open and
-        time_since_modified > time_since_modified_threshold then
-      vim.api.nvim_buf_delete(buf.bufnr, {})
-    end
-  end
-end))
-
 set_options {
   number = true,
   relativenumber = true,
@@ -75,27 +58,26 @@ if vim.fn.executable('rg') then
   }
 end
 
-map {
-  n = {
-    ['<leader>bk']    = ':lua vim.b.keep_open = true<CR>',
-    ['<leader>ff']    = ':Files<CR>',
-    ['<leader>fb']    = ':Buffers<CR>',
-    ['<leader>f.']    = ':Dotfiles<CR>',
-    ['<leader>bd']    = ':Sayonara!<CR>',
-    ['<leader>bD']    = ':Sayonara<CR>',
-    ['[h']            = ':GitGutterPrevHunk<CR>',
-    [']h']            = ':GitGutterNextHunk<CR>',
-    ['<leader>gg']    = ':Git<CR>',
-    ['<leader>gb']    = ':Git blame<CR>',
-    ['<leader>gl']    = ':Git log<CR>',
-    ['<leader><Tab>'] = ':NvimTreeToggle<CR>',
-    ['-']             = ':NvimTreeFindFile<CR>'
-  }
-}
+ map {
+   n = {
+     ['<leader>ff']    = ':Files<CR>',
+     ['<leader>fb']    = ':Buffers<CR>',
+     ['<leader>f.']    = ':Dotfiles<CR>',
+     ['[h']            = ':GitGutterPrevHunk<CR>',
+     [']h']            = ':GitGutterNextHunk<CR>',
+     ['<leader>gg']    = ':Git<CR>',
+     ['<leader>gb']    = ':Git blame<CR>',
+     ['<leader>gl']    = ':Git log<CR>',
+     ['<C-s>']         = ':BufferPick<CR>',
+     ['<leader>bd']    = ':BufferClose<CR>',
+     ['<leader><Tab>'] = ':NvimTreeToggle<CR>',
+     ['-']             = ':NvimTreeFindFile<CR>'
+   }
+ }
  
 map({ n = {
   ['<LEADER>fF'] = ':Files ',
   ['<LEADER>fg'] = ':Grep ',
 }}, {silent = false})
 
-vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')
+vim.cmd('autocmd TermOpen * setlocal nonumber')
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,22 +10,14 @@ return require('packer').startup(function()
   use {'wbthomason/packer.nvim', opt = true}
   use {'neovim/nvim-lspconfig', config = require('config.lsp')}
   use {'nvim-lua/completion-nvim'}
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    config = require('config.treesitter')
-  }
   use {'junegunn/fzf.vim', config = require('config.fzf')}
   use {'junegunn/fzf', run = vim.fn['fzf#install']}
-
   use {'tpope/vim-commentary'}
   use {'tpope/vim-sleuth'}
   use {'tpope/vim-surround'}
   use {'tpope/vim-unimpaired'}
   use {'tpope/vim-fugitive'}
-  use {'tpope/vim-dispatch', config = require('config.dispatch')}
   use {'wellle/targets.vim'}
-  use {'mhinz/vim-sayonara'}
   use {'morhetz/gruvbox'}
   use {'airblade/vim-gitgutter'}
   use {'justinmk/vim-sneak'}
@@ -36,4 +28,5 @@ return require('packer').startup(function()
   use {'mfussenegger/nvim-jdtls'}
   use {'kyazdani42/nvim-web-devicons'}
   use {'kyazdani42/nvim-tree.lua'}
+  use {'romgrk/barbar.nvim'}
 end)
