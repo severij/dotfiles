c7c214c1d36353aef0ebf4708b40dc3e45eca336
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2022-08-23 13:20:37 +0300

    Add new Telescope mappings

--- neovim/.config/nvim/lua/config/telescope.lua
+++ neovim/.config/nvim/lua/config/telescope.lua
@@ -24,4 +24,8 @@ end
 
 
 vim.keymap.set('n', '<Leader>ff', builtin.find_files)
+vim.keymap.set('n', '<Leader>fg', builtin.live_grep)
+vim.keymap.set('n', '<Leader>fb', builtin.buffers)
+vim.keymap.set('n', '<Leader>fr', builtin.oldfiles)
+vim.keymap.set('n', '<Leader>fc', builtin.commands)
 vim.keymap.set('n', '<Leader>f.', find_dotfiles)
