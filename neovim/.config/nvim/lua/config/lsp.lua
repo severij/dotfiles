06d3ba8eb89379f24e2497132427e05833e6affa
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2022-08-23 13:17:42 +0300

    Add clojure_lsp to LSP config

--- neovim/.config/nvim/lua/config/lsp.lua
+++ neovim/.config/nvim/lua/config/lsp.lua
@@ -35,6 +35,8 @@ lspconfig.sumneko_lua.setup {
   },
 }
 
+lspconfig.clojure_lsp.setup{}
+
 lspconfig.rust_analyzer.setup {
     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
