cff840bf0f5c46a81528bc3377fdc1fa99df718e
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2022-08-30 18:37:44 +0300

    Use <C-Space> for both completion and confirmation

--- neovim/.config/nvim/lua/config/cmp.lua
+++ neovim/.config/nvim/lua/config/cmp.lua
@@ -14,13 +14,19 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-Space>'] = function(fallback)
+        if cmp.visible() then
+          return cmp.confirm { select = true } or fallback()
+        else
+          return cmp.complete() or fallback()
+        end
+      end,
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
       { name = 'luasnip' },
+      { name = 'nvim_lsp' },
       { name = 'path' },
     }, {
       { name = 'buffer' },
