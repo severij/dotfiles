be982399ef342ad2375f7d245908da82bca06063
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2022-09-19 12:30:28 +0300

    Set `shiftwidth` for Clojure

--- neovim/.config/nvim/ftplugin/clojure.lua
+++ neovim/.config/nvim/ftplugin/clojure.lua
@@ -1,3 +1,5 @@
+vim.opt.shiftwidth = 2
+
 vim.keymap.set({'n', 'v'}, '[[', function() vim.fn.search('^(', 'bW') end)
 vim.keymap.set({'n', 'v'}, ']]', function() vim.fn.search('^(') end)
 vim.keymap.set({'n', 'v'}, '[]', function() vim.fn.search(')\\n\\s*\\n', 'bW') end)
