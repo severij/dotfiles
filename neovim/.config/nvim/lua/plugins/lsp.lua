396507d8e0e9df73977e3b71fc29640ac1e2e569
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2025-04-25 12:59:39 +0300

    nvim: enable `ts_ls`

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
   'neovim/nvim-lspconfig',
   config = function()
-    vim.lsp.enable({'clojure_lsp', 'lua_ls', 'pylsp', 'tsserver'})
+    vim.lsp.enable({'clojure_lsp', 'lua_ls', 'pylsp', 'ts_ls'})
   end
 }
