67b5b0a41516e8249a2fd23c50004c728c80def9
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2025-08-29 18:23:23 +0300

    nvim: add `markview` plugin

---
+++ neovim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+  opts = {
+    preview = {
+      hybrid_modes = { 'i' },
+      linewise_hybrid_mode = true,
+      filetypes = { 'markdown', 'codecompanion' },
+      ignore_buftypes = {}
+    }
+  }
+}
