e3b3ba9a8a2a6feee98a771441e46357519cd7c1
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2026-06-01 10:49:23 +0300

    nvim: update plugins (migration to nvim version `0.12.2`)

--- neovim/.config/nvim/lua/plugins/surround.lua
+++ neovim/.config/nvim/lua/plugins/surround.lua
@@ -1,19 +1,5 @@
 return {
   'kylechui/nvim-surround',
   version = '*',
-  event = 'VeryLazy',
-  config = function() require('nvim-surround').setup {
-    keymaps = {
-      insert = '<C-g>s',
-      insert_line = '<C-g>S',
-      normal = 'ys',
-      normal_cur = 'yss',
-      normal_line = 'yS',
-      normal_cur_line = 'SS',
-      visual = 'ys',
-      visual_line = 'yS',
-      delete = 'ds',
-      change = 'cs'
-      }
-  } end
+  event = 'VeryLazy'
 }
