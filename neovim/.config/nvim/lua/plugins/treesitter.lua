0b7f78e15ac89ee5cb6156ab2426fa403c5a3d5d
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2026-09-22 15:05:09 +0300

    nvim: update treesitter config

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,34 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  dependencies = {'nvim-treesitter/nvim-treesitter-context'},
+  branch = 'main',
+  lazy = false,
+  dependencies = { 'nvim-treesitter/nvim-treesitter-context' },
   build = ':TSUpdate',
   config = function()
-    require('nvim-treesitter.configs').setup {
-      highlight = { enable = true },
-      ensure_installed = {
-        'bash',
-        'java',
-        'regex',
-        'vimdoc',
-        'luadoc',
-        'vim',
-        'lua',
-        'markdown',
-        'query'
-      }
+    local languages = {
+      'bash',
+      'java',
+      'regex',
+      'vimdoc',
+      'luadoc',
+      'vim',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'query',
     }
+
+    require('nvim-treesitter').setup {
+      install_dir = vim.fn.stdpath('data') .. '/site',
+    }
+
+    require('nvim-treesitter').install(languages)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = languages,
+      callback = function(args)
+        vim.treesitter.start(args.buf)
+      end,
+    })
   end
 }
